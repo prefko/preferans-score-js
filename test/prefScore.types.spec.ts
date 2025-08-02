2ab122cd9c344d82e0d4099b034694b2ea87890c
Predrag Stojadinovic <predrags@nvidia.com>
2025-08-02 17:14:58 +0200

    chore: cleanup

---
+++ test/prefScore.types.spec.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+describe('prefScore.types tests', () => {
+	// TODO: implement tests
+	it.todo('should be implemented');
+});
---
+++ test/prefScoreHand.spec.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+describe('prefScoreHand tests', () => {
+	// TODO: implement tests
+	it.todo('should be implemented');
+});
